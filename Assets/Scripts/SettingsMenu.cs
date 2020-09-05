c5f0a795c084245bd3d74c8524ab778f8d84862d
AndyWu <otw23@cam.ac.uk>
2020-09-05 23:08:49 +0800

    Added Title Screen and Settings

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        int currentResIndex = 0;
+
+        resolutionDropdown.ClearOptions();
+
+        Debug.Log(Screen.width + " x " + Screen.height);
+        for (int i=0; i<resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width + " x " + resolutions[i].height);
+            if (Screen.width == resolutions[i].width && Screen.height == resolutions[i].height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void ChangeResolution(int resolutionIndex)
+    {
+        Resolution resoultion = resolutions[resolutionIndex];
+        Screen.SetResolution(resoultion.width, resoultion.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
